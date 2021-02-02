f5c7d49a3554c38799f3380c4a28182a549581e7
Pranshu Maheshwari <maheshwari.pranshu511@gmail.com>
2021-02-02 14:01:19 +0530

    chore: added basic typescript rn boilerplate

---
+++ app/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	useTabs: true,
+	semi: false,
+	printWidth: 140,
+	tabWidth: 4,
+}
---
+++ app/App.tsx
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import 'react-native-gesture-handler'
+
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet } from 'react-native'
+
+import { Colors } from 'react-native/Libraries/NewAppScreen'
+import { NavigationContainer } from '@react-navigation/native'
+import React from 'react'
+
+const styles = StyleSheet.create({
+	scrollView: {
+		backgroundColor: Colors.lighter,
+	},
+})
+
+const App = () => {
+	return (
+		<NavigationContainer>
+			<StatusBar barStyle="dark-content" />
+			<SafeAreaView>
+				<ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView} />
+			</SafeAreaView>
+		</NavigationContainer>
+	)
+}
+
+export default App
