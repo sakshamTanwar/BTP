2627b8361a70843cdb450e894cc9b551fc6632a6
Naman Jain <17namanjain@gmail.com>
2021-02-11 21:10:42 +0530

    Add Html view for land coordinate resolver

---
+++ server/src/interfaces/httpinterfaces.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction} from 'express';
+import {IUser} from "../models/user"
+ 
+export interface IRequest extends Request {
+	user: IUser;
+}
+
+export interface IResponse extends Response {}
+export interface INext extends NextFunction {}
