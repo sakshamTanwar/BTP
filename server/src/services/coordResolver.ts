6a8299141b608d1d7eee5daa07392a3b6c4e8bb8
Naman Jain <17namanjain@gmail.com>
2021-02-16 15:37:07 +0530

    Replace LocationIQ reverse geocoding with MapMyIndia reverse geocoding

--- server/src/services/coordResolver.ts
+++ server/src/services/coordResolver.ts
@@ -3,25 +3,29 @@ import { IResolvedCoordInfo } from "../interfaces/coordResolverInterfaces";
 
 export function resolveCoords(lat:any, lon:any){
     
-    const api_key = "pk.f290968746686314a1c4977840cc0353";
-    const api_url = `https://us1.locationiq.com/v1/reverse.php?key=${api_key}&lat=${lat}&lon=${lon}&format=json`;
+    // const api_key = "pk.f290968746686314a1c4977840cc0353";
+    // const api_url = `https://us1.locationiq.com/v1/reverse.php?key=${api_key}&lat=${lat}&lon=${lon}&format=json`;
    
+    const api_key = "nup6q766plnyngyj17u844z79fzjc45y";
+    const api_url = `https://apis.mapmyindia.com/advancedmaps/v1/${api_key}/rev_geocode?lat=${lat}&lng=${lon}`;
     return new Promise((resolve, reject) => {
         
         fetch(api_url)
         .then(res => res.json())
         .then(data => {
-
+            
+            data = data.results[0];
             let resolvedInfo: IResolvedCoordInfo = {
                 point: {
                     lat: lat,
                     lon: lon
                 },
-                country: data.address.country,
-                state: data.address.state,
-                district: data.address.state_district,
-                subDistrict: data.address.county,
-                village: data.address.village,
+                
+                country: data.area,
+                state: data.state,
+                district: data.district,
+                subDistrict: data.subDistrict,
+                village: data.village,
             }       
                 
             console.log(resolvedInfo.village);
