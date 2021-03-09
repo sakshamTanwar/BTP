94fc4408ae263007784543e1f8d962e4768a20ec
Saksham Tanwar <saksham.tanwar608@gmail.com>
2021-03-09 09:47:24 +0530

    Setup basic express app

--- blockchain/application-land-reg/src/app.ts
+++ blockchain/application-land-reg/src/app.ts
@@ -1,48 +1,21 @@
-import inquirer from 'inquirer';
-import { promptAddLand } from './prompts/addLand';
-import { promptQueryVillage } from './prompts/queryAllVillageRecords';
-import { promptOwnershipHistory } from './prompts/queryOwnershipHistory';
-import { promptSplitLand } from './prompts/splitLand';
-import { promptTransferLand } from './prompts/transferLand';
+import express, { Request, Response, NextFunction } from 'express';
 
-async function showMenu() {
-    while (true) {
-        const quesMenu = {
-            type: 'list',
-            name: 'menu',
-            message: 'What do you want to do?',
-            choices: [
-                { name: 'Add Land Record', value: 1 },
-                { name: 'Transfer Land Ownership', value: 2 },
-                { name: 'Split Land', value: 3 },
-                { name: 'Query All Records in a Village', value: 4 },
-                { name: 'Query Ownership History of Land', value: 5 },
-                { name: 'Exit', value: 6 },
-            ],
-        };
+const app = express();
+const port = process.env.port || 8080;
 
-        let menuChoice = await inquirer.prompt([quesMenu]);
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    let statusCode = err.statusCode || 500;
+    let errorCode = err.error || 'server_err';
+    let message = err.message || 'Internal Server Error';
+    res.status(statusCode).send({
+        success: false,
+        error: {
+            code: errorCode,
+            message: message,
+        },
+    });
+});
 
-        switch (menuChoice.menu) {
-            case 1:
-                await promptAddLand();
-                break;
-            case 2:
-                await promptTransferLand();
-                break;
-            case 3:
-                await promptSplitLand();
-                break;
-            case 4:
-                await promptQueryVillage();
-                break;
-            case 5:
-                await promptOwnershipHistory();
-                break;
-            default:
-                process.exit();
-        }
-    }
-}
-
-showMenu();
+app.listen(port, () => {
+    console.log(`server started at http://localhost:${port}`);
+});
