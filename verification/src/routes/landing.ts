b9fe45f3d39985f155f1b2f1442367b8f959f234
Naman Jain <17namanjain@gmail.com>
2021-03-16 23:15:15 +0530

    Verify and sign certificate

--- verification/src/routes/landing.ts
+++ verification/src/routes/landing.ts
@@ -1,4 +1,7 @@
 import express from 'express'
+import verifyCertificate from '../services/certificate/verify'
+import signPDF from '../services/certificate/signPDF'
+import path from 'path'
 
 const uint8ArrayConcat = require('uint8arrays/concat')
 
@@ -24,12 +27,30 @@ router.get(`/`, (_, res) => {
 router.post(`/`, async (req, res, next) => {
 	try {
 		const { hash } = req.body
+		if(!hash){
+			throw new Error("Invalid Hash");
+		}
+		
+		let certRecord = await verifyCertificate(hash, 'LAND');
+		if(certRecord.length==0){
+			throw new Error("Invalid Hash");
+		}
+
 		const node = IpfsHttpClient()
 		const chunks: Uint8Array[] = []
+		let a = node.cat(new CID(hash));
+		console.log(typeof(a));
 		for await (const chunk of node.cat(new CID(hash))) {
+			console.log(typeof(chunk), chunk);
 			chunks.push(chunk)
 		}
-		readStream.end(uint8ArrayConcat(chunks))
+        let pdfBuffer = Buffer.concat(chunks); 
+        let signedPdfBuffer = signPDF(pdfBuffer, path.join(__dirname,'..','..','certificate.p12'));
+
+		// readStream.end(uint8ArrayConcat(chunks))
+		// console.log(uint8ArrayConcat(chunks));
+		readStream.end(signedPdfBuffer)
+		console.log(signedPdfBuffer);
 		res.set('Content-Type', 'application/pdf')
 		readStream.pipe(res)
 	} catch (err) {
