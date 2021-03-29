0a2304227d7c7277c5c070fd2b527d5ea92186c5
Saksham Tanwar <saksham.tanwar608@gmail.com>
2021-03-29 10:53:26 +0530

    Export API endpoint to verify a payment and send email to user in LRSP server

--- server/src/routes/payment.ts
+++ server/src/routes/payment.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import passport from 'passport';
 import { PaymentHandler } from '../services/paymentHandler';
+import { PDFGenerator } from '../services/pdfGenerator';
+import { queryOwnershipHistory } from '../blockchain/queryOwnershipHistory';
+import path from 'path';
+import { EmailHandler } from '../services/emailHandler';
 
 const router = express.Router();
 
@@ -27,6 +31,91 @@ router.get(
             res.json({
                 order_id: orderDetails.id,
             });
+        } catch (err) {
+            next(err);
+        }
+    },
+);
+
+function sendPdf(
+    email: string,
+    khasra: string,
+    village: string,
+    subDistrict: string,
+    district: string,
+    state: string,
+) {
+    queryOwnershipHistory(
+        khasra as string,
+        village as string,
+        subDistrict as string,
+        district as string,
+        state as string,
+    )
+        .then(async records => {
+            let nKhasra = (khasra as string).replace(/\//g, '_');
+            let pdfPath = path.join(
+                __dirname,
+                '..',
+                '..',
+                'temp',
+                `${nKhasra}_${village}.pdf`,
+            );
+            PDFGenerator.generatePDF(records, pdfPath);
+            console.log('PDF Generated');
+            return pdfPath;
+        })
+        .then(pdfPath => {
+            EmailHandler.mailPdf(email, 'landRecord.pdf', pdfPath);
+        })
+        .catch(err => {
+            throw err;
+        });
+}
+
+router.get(
+    '/verify',
+    passport.authenticate('jwt', { session: false }),
+    async (req, res, next) => {
+        let {
+            order_id,
+            razorpay_order_id,
+            razorpay_payment_id,
+            razorpay_signature,
+        } = req.query;
+
+        if (
+            !order_id ||
+            !razorpay_order_id ||
+            !razorpay_payment_id ||
+            !razorpay_signature
+        ) {
+            res.status(400).send('Invalid Data');
+        }
+
+        try {
+            await PaymentHandler.registerPayment(
+                order_id as string,
+                razorpay_order_id as string,
+                razorpay_payment_id as string,
+                razorpay_signature as string,
+            );
+        } catch (err) {
+            next(err);
+        }
+
+        try {
+            const orderDetails = await PaymentHandler.getOrderDetails(
+                order_id as string,
+            );
+            sendPdf(
+                orderDetails.user_email,
+                orderDetails.land_khasra,
+                orderDetails.land_village,
+                orderDetails.land_sub_district,
+                orderDetails.land_district,
+                orderDetails.land_state,
+            );
         } catch (err) {
             next(err);
         }
--- server/src/services/paymentHandler.ts
+++ server/src/services/paymentHandler.ts
@@ -1,5 +1,8 @@
 import Razorpay from 'razorpay';
 import util from 'util';
+import crypto from 'crypto';
+import { Payment } from '../models/payment';
+import fetch from 'node-fetch';
 
 export class PaymentHandler {
     private static razorpayInstance = new Razorpay({
@@ -38,5 +41,73 @@ export class PaymentHandler {
         const order = await orderCreatePromise(options);
 
         return order;
+    }
+
+    private static verifySignature(
+        order_id: string,
+        razorpay_payment_id: string,
+        razorpay_signature: string,
+    ) {
+        return (
+            crypto
+                .createHmac('sha256', process.env.RZRPAY_KEY_SECRET)
+                .update(order_id + '|' + razorpay_payment_id)
+                .digest('hex') !== razorpay_signature
+        );
+    }
+
+    private static async isPaymentComplete(
+        order_id: string,
+        payment_id: string,
+    ) {
+        const apiURL = `https://${process.env.RZRPAY_KEY_ID}:${process.env.RZRPAY_KEY_SECRET}@api.razorpay.com/v1/payments/${payment_id}`;
+
+        const payment = JSON.parse(
+            (await fetch(apiURL)).body.read().toString(),
+        );
+
+        return payment.status === 'captured' && payment.order_id === order_id;
+    }
+
+    public static async registerPayment(
+        order_id: string,
+        razorpay_order_id: string,
+        razorpay_payment_id: string,
+        razorpay_signature: string,
+    ) {
+        if (
+            !this.verifySignature(
+                order_id,
+                razorpay_payment_id,
+                razorpay_signature,
+            )
+        ) {
+            throw Error('Invalid payment signature');
+        }
+
+        const completed = await this.isPaymentComplete(
+            order_id,
+            razorpay_payment_id,
+        );
+
+        if (!completed) {
+            throw Error('Payment not complete');
+        }
+
+        let payment = new Payment({
+            order_id: razorpay_order_id,
+            payment_id: razorpay_payment_id,
+            signature: razorpay_signature,
+        });
+
+        await payment.save();
+    }
+
+    public static async getOrderDetails(order_id: string) {
+        const apiURL = `https://${process.env.RZRPAY_KEY_ID}:${process.env.RZRPAY_KEY_SECRET}@api.razorpay.com/v1/orders/${order_id}`;
+
+        const order = JSON.parse((await fetch(apiURL)).body.read().toString());
+
+        return order.notes;
     }
 }
